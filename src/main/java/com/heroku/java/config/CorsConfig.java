893615a51b9c010801e6cdf2120f8fe0d0c43ad6
sahandrade <92736761+sahandrade@users.noreply.github.com>
2023-09-18 08:55:58 -0300

    m

---
+++ src/main/java/com/heroku/java/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.heroku.java.config;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
+
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+
+        // You can configure other CORS settings as needed
+        // configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        // configuration.setAllowCredentials(true);
+        // configuration.addAllowedHeader("Authorization");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
