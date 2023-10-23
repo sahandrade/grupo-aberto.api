1ab09f5aa2b52d154cf7e50206a84f1bc47b60a3
sahandrade <92736761+sahandrade@users.noreply.github.com>
2023-10-23 15:32:16 -0300

    Praticando_5

--- src/main/java/com/heroku/java/controller/LivroController.java
+++ src/main/java/com/heroku/java/controller/LivroController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,9 @@ public class LivroController {
         return livroRepository.findAll();
     }
 
-
+    @GetMapping(value = "/{id}")
+public Livro BuscarLivroPorId(@PathVariable String id) {
+   return livroRepository.getById(id);
+}
     
 }
--- src/main/java/com/heroku/java/repository/interfaces/ILivroRepository.java
+++ src/main/java/com/heroku/java/repository/interfaces/ILivroRepository.java
@@ -1,9 +1,12 @@
 package com.heroku.java.repository.interfaces;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
+import com.heroku.java.model.Conta;
 import com.heroku.java.model.Livro;
 
 public interface ILivroRepository extends MongoRepository<Livro, String>{
-    
+    @Query(value="{id:'?0'}")
+    Livro getById(String id);
 }
